2373f54b0d5f2652a8b1a84474dd348b293d7c86
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-02-09 23:26:34 +0100

    added fullscan and scan

--- test/test.js
+++ test/test.js
@@ -2,11 +2,14 @@
 const expect = require('chai').expect;
 const fs = require('fs');
 const dree = require('../lib/index');
+const args = process.argv;
 
 describe('Dree module tests', () => {
 
     const path = 'test/sample';
-    require('./scan/scan.test')(expect, fs, dree, path);
+    if(args.find(arg => arg === '--scan')) {
+        require('./scan/scan.test')(expect, fs, dree, path);
+    }
     require('./parse/parse.test')(expect, dree, path);
     require('./parseTree/parseTree.test')(expect, dree, path);
 });
