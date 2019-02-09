deeceb046e392f830bd639d33c0f96308da6dfc2
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-02-09 17:59:33 +0100

    added documentation to interfaces

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -26,44 +26,157 @@ declare class Stats {
     ctime: Date;
     birthtime: Date;
 }
+/**
+ * Enum whose values are DIRECTORY or FILE
+ */
 export declare enum Type {
     DIRECTORY = "directory",
     FILE = "file"
 }
+/**
+ * Interface of an object representing a Directory Tree
+ */
 export interface Dree {
+    /**
+     *  The name of the node as a string
+     */
     name: string;
+    /**
+     * The absolute path of the node
+     */
     path: string;
+    /**
+     * The relative path from the root of the node
+     */
     relativePath: string;
+    /**
+     * Values: Type.DIRECTORY or Type.FILE
+     */
     type: Type;
+    /**
+     * A boolean with true value if the node is a symbolic link
+     */
     isSymbolicLink: boolean;
+    /**
+     * Optional. The size in bytes of the node
+     */
     sizeInBytes?: number;
+    /**
+     * Optional. The size of the node, rounded to two decimals and appropriate unit
+     */
     size?: string;
+    /**
+     * Optional. The hash of the node
+     */
     hash?: string;
+    /**
+     * Optional. The extension (without dot) of the node. Returned only if the node is a file
+     */
     extension?: string;
+    /**
+     * Optional. The fs.lstat or fs.fstat of the node
+     */
     stat?: Stats;
+    /**
+     * Optional. An array of Dree objects, containing all the children of the node
+     */
     children?: Dree[];
 }
+/**
+ * Interface of the options object used with "scan" function
+ */
 export interface ScanOptions {
+    /**
+     * If true every node of the result will contain stat property, provided by fs.lstat or fs.stat
+     */
     stat?: boolean;
+    /**
+     * If true, on windows, normalize each path replacing each backslash \\\\ with a slash /
+     */
     normalize?: boolean;
+    /**
+     * If true, all symbolic links found will be included in the result. Could not work on Windows
+     */
     symbolicLinks?: boolean;
+    /**
+     * If true, all symbolic links will be followed, including even their content if they link to a folder.
+     * Could not work on Windows
+     */
     followLinks?: boolean;
+    /**
+     * If true, every node in the result will contain sizeInBytes property as the number of bytes of the content.
+     * If a node is a folder, only its considered inner files will be computed to have this size
+     */
     sizeInBytes?: boolean;
+    /**
+     * If true, every node in the result will contain size property. Same as sizeInBytes, but it
+     * is a string rounded to the second decimal digit and with an appropriate unit
+     */
     size?: boolean;
+    /**
+     * If true, every node in the result will contain hash property, computed by taking in consideration
+     * the name and the content of the node. If the node is a folder, all his considered inner files will be used by the algorithm
+     */
     hash?: boolean;
+    /**
+     * Hash algorithm used by cryptojs to return the hash
+     */
     hashAlgorithm?: 'md5' | 'sha1';
+    /**
+     * Hash encoding used by cryptojs to return the hash
+     */
     hashEncoding?: HexBase64Latin1Encoding;
+    /**
+     * If true, all hidden files and dirs will be included in the result. A hidden file or a directory
+     * has a name wich starts with a dot and in some systems like Linux are hidden
+     */
     showHidden?: boolean;
+    /**
+     * It is a number wich says the max depth the algorithm can reach scanning the given path.
+     * All files and dirs wich are beyound the max depth will not be considered by the algorithm
+     */
     depth?: number;
+    /**
+     * It is a regex or array of regex and all the matched paths will not be considered by the algorithm
+     */
     exclude?: RegExp | RegExp[];
+    /**
+     * It is an array of strings and all the files whose extension is not included in that array will be skipped by the algorithm.
+     * If value is undefined, all file extensions will be considered, if it is [], no files will be included
+     */
     extensions?: string[];
 }
+/**
+ * Interface of the options object used with "parse" or "parseTree" functions
+ */
 export interface ParseOptions {
+    /**
+     * If true, all symbolic links found will be included in the result. Could not work on Windows
+     */
     symbolicLinks?: boolean;
+    /**
+     * If true, all symbolic links will be followed, including even their content if they link to a folder.
+     * Could not work on Windows
+     */
     followLinks?: boolean;
+    /**
+     * If true, all hidden files and dirs will be included in the result. A hidden file or a directory
+     * has a name wich starts with a dot and in some systems like Linux are hidden
+     */
     showHidden?: boolean;
+    /**
+     * It is a number wich says the max depth the algorithm can reach scanning the given path.
+     * All files and dirs wich are beyound the max depth will not be considered by the algorithm
+     */
     depth?: number;
+    /**
+     * It is a regex or array of regex and all the matched paths will not be considered by the algorithm
+     */
     exclude?: RegExp | RegExp[];
+    /**
+     * It is an array of strings and all the files whose extension is not included in that array will be skipped by the algorithm.
+     * If value is undefined, all file extensions will be considered, if it is [], no files will be included
+     */
     extensions?: string[];
 }
 export declare type Callback = (dirTree: Dree, stat: Stats) => void;
