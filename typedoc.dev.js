56fbb3c2345738cf9479c9049701328fcbb81711
euberdeveloper <euberdeveloper@gmail.com>
2020-12-30 19:20:29 +0100

    docs: remove ltypedoc

--- typedoc.dev.js
+++ typedoc.dev.js
@@ -1,8 +1,8 @@
 module.exports = {
-    mode: 'modules',
+    entryPoints: [
+        './source'
+    ],
     name: 'dree - DEV',
-    entryPoint: 'source/lib/index.ts',
-    includeVersion: true,
     tsconfig: 'source/tsconfig.json',
     gaID: process.env.GA_TOKEN,
     out: './docs/documentation/html-dev'
