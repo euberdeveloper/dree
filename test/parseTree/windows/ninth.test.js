2eb3da7916285d787fd5b2c27d389c2b5685ebb1
euberdeveloper <euberdeveloper@gmail.com>
2022-10-17 00:34:18 +0200

    test: update windows expecteds

---
+++ test/parseTree/windows/ninth.test.js
@@ -0,0 +1,13 @@
+module.exports =
+`sample
+ ├── .gitignore
+ ├─> backend
+ │   ├── linked.link
+ │   ├── linkedtxt.link
+ │   └─> server
+ │       └── server.ts
+ ├─> empty
+ │   └─> tsempty
+ │       └── file.ts
+ └─> linked
+     └── linked.txt`;
